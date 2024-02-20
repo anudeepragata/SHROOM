c931a05ddb89d37f81f0da23a1abdc08d945b3cf
anudeepragata <72305449+anudeepragata@users.noreply.github.com>
2024-02-20 17:21:43 -0500

    added parsing code

---
+++ model_aware_shroom_parse.py
@@ -0,0 +1,20 @@
+import json
+ 
+# Opening JSON file
+f = open('SHROOM_unlabeled-training-data-v2/train.model-aware.v2.json')
+ 
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+ 
+# Iterating through the json
+# list
+parsed_data = []
+for i in data:
+    datum = {}
+    for key in i:
+        datum[key] = i[key]
+    parsed_data.append(datum)
+ 
+# Closing file
+f.close()
